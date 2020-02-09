c1f754689a6af6feab346d7bff36ae73cf241032
sasaplus1 <sasaplus1@gmail.com>
2020-02-09 21:16:09 +0900

    add test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 var assert = require('assert');
+var http = require('http');
 var path = require('path');
+
+var express = require('express');
 
 var expressFreemarker = require('../');
 
@@ -66,6 +69,52 @@ describe(metadata.name, function() {
 
       assert(typeof renderer === 'function');
       assert(renderer.length === 3);
+    });
+  });
+  describe('use with Express', function() {
+    it('should response rendered HTML', function(done) {
+      var app = express();
+      var port = 3939;
+      var server;
+
+      app.engine('ftl', expressFreemarker.engine);
+      app.set('view engine', 'ftl');
+      app.set('views', path.join(__dirname, 'engine/templates'));
+
+      app.get('/', function(req, res) {
+        res.render('main/index.ftl');
+      });
+
+      server = app.listen(port, function() {
+        var url = 'http://127.0.0.1:' + port + '/';
+
+        http
+          .request(url, function(res) {
+            var buffer = '';
+
+            if (res.statusCode < 200 || res.statusCode > 300) {
+              return done(new Error('response code is not 2xx'));
+            }
+
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+              buffer += chunk;
+            });
+            res.on('end', function() {
+              assert(buffer === '<header>It Works!</header>\n<p>Hello!</p>\n');
+
+              server.close();
+
+              done();
+            });
+          })
+          .on('error', function(err) {
+            server.close();
+
+            done(err);
+          })
+          .end();
+      });
     });
   });
 });
