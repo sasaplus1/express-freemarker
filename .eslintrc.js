dc7428e9be6239644627072ec4e2a659d3ff2c1f
sasaplus1 <sasaplus1@gmail.com>
2020-10-14 19:11:36 +0900

    setup lint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,44 @@
 module.exports = {
   env: {
+    es6: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:node/recommended-script', 'prettier'],
   overrides: [
     {
+      extends: [
+        'eslint:recommended',
+        'plugin:node/recommended-module',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint'
+      ],
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint']
+    },
+    {
       env: {
+        es6: true,
         mocha: true
       },
-      files: ['test/**/*.js']
+      extends: [
+        'eslint:recommended',
+        'plugin:node/recommended-module',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint'
+      ],
+      files: ['test/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint']
     }
   ],
   parserOptions: {
-    ecmaVersion: 3
+    ecmaVersion: 2020,
+    sourceType: 'module'
   },
   root: true
 };
